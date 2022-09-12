259ed7cdc399d2ccc76e33f0cddadb266b8a7921
Nitin <nitin@Nitins-MacBook-Pro.local>
2022-09-12 11:26:18 +0530

    added funtionality for updating restaurants

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,22 +1,56 @@
 import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 const AddRestaurant = () => {
+  const { addRestaurants } = useContext(RestaurantsContext);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await RestaurantFinder.post("/", {
+        name,
+        location,
+        price_range: priceRange,
+      });
+
+      addRestaurants(response.data.data.restaurant);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="mb-4">
       <form action="">
         <div className="form-row  d-flex  ">
           <div className="col  p-3 ">
-            <input type="text" className="form-control " placeholder="name" />
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className="form-control "
+              placeholder="name"
+            />
           </div>
           <div className="col p-3 ">
             <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               type="text"
               className="form-control "
               placeholder="location"
             />
           </div>
           <div className="col p-3 ">
-            <select className="custom-select  mr-sm-2 w-100 h-100 form-control rounded ">
+            <select
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="custom-select  mr-sm-2 w-100 h-100 form-control rounded "
+            >
               <option disabled>Price Range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
@@ -25,7 +59,13 @@ const AddRestaurant = () => {
               <option value="5">$$$$$</option>
             </select>
           </div>
-          <button className="btn btn-primary  my-3 ml-3">Add</button>
+          <button
+            onClick={handelSubmit}
+            type="form"
+            className="btn btn-primary  my-3 ml-3"
+          >
+            Add
+          </button>
         </div>
       </form>
     </div>
--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import { useContext } from "react";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useNavigate } from "react-router-dom";
 
 export const RestaurantList = (props) => {
   const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+
+  let navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +18,25 @@ export const RestaurantList = (props) => {
     };
 
     fetchData();
-  }, []);
+  }, [setRestaurants]);
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await RestaurantFinder.delete(`/${id}`);
+      setRestaurants(
+        restaurants.filter((restaurant) => {
+          return restaurant.id !== id;
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdate = (id) => {
+    navigate(`/restaurants/${id}/update`);
+  };
+
   return (
     <div className="list-group">
       <table className="table table-hover ">
@@ -30,7 +51,34 @@ export const RestaurantList = (props) => {
           </tr>
         </thead>
         <tbody className=" table-dark">
-          <tr>
+          {restaurants &&
+            restaurants.map((restaurant) => {
+              return (
+                <tr key={restaurant.id}>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.location}</td>
+                  <td>{"$".repeat(restaurant.price_range)}</td>
+                  <td>reviews</td>
+                  <td>
+                    <button
+                      onClick={() => handleUpdate(restaurant.id)}
+                      className="btn btn-warning"
+                    >
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(restaurant.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          {/* <tr>
             <td>mcdonals</td>
             <td>New York</td>
             <td>$$</td>
@@ -53,7 +101,7 @@ export const RestaurantList = (props) => {
             <td>
               <button className="btn btn-danger">Delete</button>
             </td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </div>
